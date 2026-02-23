d1c79eb5a5df14f9f9f4d66cce3c74d39a9585b9
Tymon Mikula <karbrz01@easv365.dk>
2026-02-23 09:47:26 +0100

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import dotenvFlow from 'dotenv-flow';
 import {connect, testConnection} from './repository/database';
-
+import cors from 'cors';
 import routes from './routes';
 import { disconnect } from './repository/database';
 
@@ -10,6 +10,22 @@ const app: Application = express();
 
 
 export function startServer() {
+
+    app.use(cors({
+
+    // Allow request from any origin
+    origin: "*",
+
+    // allow HTTP methods
+    methods: ["GET", "PUT", "POST", "DELETE"],
+
+    // allow headers
+    allowedHeaders: ['auth-token', 'Origin', 'X-Requested-Width', 'Content-Type', 'Accept'],
+
+    // allow credentials
+    credentials:true
+    }))
+
     app.use(express.json());
     
     app.use('/api', routes);
