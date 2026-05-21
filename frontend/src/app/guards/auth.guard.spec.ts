a559d054eb7f62c291716b39aadaa0338dc9cd4c
Karolina <karbrz01@easv365.dk>
2026-05-21 11:24:57 +0200

    unit teste for auth guard

---
+++ frontend/src/app/guards/auth.guard.spec.ts
@@ -0,0 +1,54 @@
+import { TestBed } from '@angular/core/testing';
+import { describe, it, expect, beforeEach, vi } from 'vitest';
+import { Router } from '@angular/router';
+
+import { authGuard } from './auth.guard';
+import { AuthService } from '../services/auth.service';
+
+describe('authGuard', () => {
+  let authService: any;
+  let router: Router;
+
+  beforeEach(() => {
+    authService = {
+      getToken: vi.fn(),
+    };
+
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: AuthService,
+          useValue: authService,
+        },
+        {
+          provide: Router,
+          useValue: {
+            navigate: vi.fn(),
+          },
+        },
+      ],
+    });
+
+    router = TestBed.inject(Router);
+  });
+
+  it('should return true, When token exists', () => {
+    authService.getToken.mockReturnValue('token');
+
+    const result = TestBed.runInInjectionContext(() =>authGuard({} as any, {} as any));
+
+    expect(authService.getToken).toHaveBeenCalled();
+    expect(result).toBe(true);
+    expect(router.navigate).not.toHaveBeenCalled();
+  });
+
+  it('should navigate to login and return false, When token does not exist', () => {
+    authService.getToken.mockReturnValue(null);
+
+    const result = TestBed.runInInjectionContext(() => authGuard({} as any, {} as any));
+
+    expect(authService.getToken).toHaveBeenCalled();
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
+    expect(result).toBe(false);
+  });
+});
