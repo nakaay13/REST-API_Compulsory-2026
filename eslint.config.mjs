4b269ba24841fca483d134671a7e25c93ceab26c
nakaay13 <karbrz01@easv365.dk>
2026-05-30 17:01:16 +0200

    added linter to the project

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    //tseslint.configs.recommendedTypeChecked,
+    tseslint.configs.stylistic,
+    {
+        ignores: [
+            'node_modules/',
+            'dist/', // exclude specific folder
+            '**/*.js', // exclude all JavaScript files
+            '**/*.mjs'
+        ]
+    },
+    {
+        files: ['**/*.ts'],   
+    },
+]
+);
